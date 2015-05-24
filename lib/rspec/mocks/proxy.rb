ec54251834b8b3d73908268820d836cacf77f68f
Sam Phippen <samphippen@googlemail.com>
2015-05-24 12:26:30 +0100

    Red

--- lib/rspec/mocks/proxy.rb
+++ lib/rspec/mocks/proxy.rb
@@ -169,11 +169,12 @@ module RSpec
         stub = find_matching_method_stub(message, *args)
 
         if (stub && expectation && expectation.called_max_times? && expectation.actual_received_count_matters?)
-            expectation.increase_actual_received_count!
+          expectation.increase_actual_received_count!
         end
 
+        almost_matching_expectation = find_almost_matching_expectation(message, *args)
+
         if stub && ((expectation && expectation.called_max_times?) || !expectation)
-          almost_matching_expectation = find_almost_matching_expectation(message, *args)
           if almost_matching_expectation && !almost_matching_expectation.expected_messages_received?
             almost_matching_expectation.advise(*args)
           end
